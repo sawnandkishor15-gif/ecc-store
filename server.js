e97a595d9eb111b7727dcdede334f8390ed9a356
sawnandkishor15-gif <sawnandkishor15@gmail.com>
2026-06-15 19:23:22 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const app = express();
 // Security Guard Middlewares
 app.use(cors());
 app.use(express.json());
+// Import route at the top
+import authRoutes from './routes/auth.js';
+
+// Add this below app.use(express.json());
+app.use('/api/auth', authRoutes);
+
 
 // ⚠️ SABSE IMP: Apni MongoDB connection string yahan paste karein password ke sath
 const MONGO_URI = "mongodb+srv://adz_admin:adzadmin123@cluster0.mongodb+srv://adz_admin:<db_password>@cluster0.8id9g6b.mongodb.net/?appName=Cluster0.mongodb.net/adz_fashion?retryWrites=true&w=majority";
